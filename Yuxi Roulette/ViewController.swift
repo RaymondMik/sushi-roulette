b5b55d9b2c057cd2f92c4cb573bd262578e60a50
Ramón Miklus <ramon.miklus@gmail.com>
2021-04-16 22:03:49 +0200

    added new icon, minor layout adjustments

--- Yuxi Roulette/ViewController.swift
+++ Yuxi Roulette/ViewController.swift
@@ -11,7 +11,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var yuxiPreviewImage: UIImageView!
     @IBOutlet weak var itemsScrollView: UIScrollView!
     @IBOutlet weak var itemsCounter: UILabel!
-        
+    @IBOutlet weak var addPreviewButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
     var scrollView: UIScrollView!
     var images = [UIImageView]()
     var selectedItem = 0
@@ -20,26 +22,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        addPreviewButton.isHidden = true
+        
         // Set the scrollView's frame to be the size of the screen
-        scrollView = UIScrollView(frame: CGRect(x: 0, y: 530, width: view.frame.width, height: view.frame.height))
+        scrollView = UIScrollView(frame: CGRect(x: 10, y: 505, width: view.frame.width, height: view.frame.height))
         // scrollView.backgroundColor = .systemTeal
         // Set the contentSize to 5 times the height of the phone's screen so that
-        scrollView.contentSize = CGSize(width: scrollView.contentSize.width, height: UIScreen.main.bounds.height*5)
+        scrollView.contentSize = CGSize(width: scrollView.contentSize.width-10, height: UIScreen.main.bounds.height*5)
         view.addSubview(scrollView)
         
     }
 
     @IBAction func spinYuxiPreview(_ sender: Any) {
-        let randomIndex = Int.random(in: 1...157)
+        let randomIndex = Int.random(in: 2...157)
         
         Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { timer in
-            let randomNumber = Int.random(in: 1...157)
+            let randomNumber = Int.random(in: 2...157)
             self.yuxiPreviewImage.image = UIImage(named: "image-\(randomNumber)")
 
             if randomNumber == randomIndex {
                 timer.invalidate()
                 self.selectedItem = randomNumber
-                print(self.selectedItem)
+                if (self.addPreviewButton.isHidden) {
+                    self.addPreviewButton.isHidden = false
+                }
             }
         }
     }
@@ -50,10 +56,10 @@ class ViewController: UIViewController {
         }
       
         images.append(UIImageView(image: UIImage(named:  "image-\(self.selectedItem)")))
-        images[self.totalItems].contentMode = .scaleAspectFit
-        let topSpacing: Int = 15
+        images[self.totalItems].contentMode = .center
+        let topSpacing: Int = 10
         let yCoord: Int = self.totalItems != 0 ? Int(200 * self.totalItems + topSpacing) : topSpacing
-        images[self.totalItems].frame = CGRect(x: 5, y: yCoord, width: Int(self.view.frame.width), height: 200)
+        images[self.totalItems].frame = CGRect(x: 0, y: yCoord, width: Int(self.view.frame.width), height: 200)
         scrollView.addSubview(images[self.totalItems])
         
         self.totalItems = self.totalItems + 1
@@ -63,7 +69,8 @@ class ViewController: UIViewController {
     @IBAction func resetItemsState(_ sender: Any) {
         self.totalItems = 0
         self.selectedItem = 0
-        self.yuxiPreviewImage.image = nil
+        self.addPreviewButton.isHidden = true
+        self.yuxiPreviewImage.image = UIImage(named: "image-1")
         images = [UIImageView]()
         itemsCounter.text = String(self.totalItems)
         let subViews = self.scrollView.subviews
